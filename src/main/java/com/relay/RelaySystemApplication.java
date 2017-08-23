b274cf2a80c57da6da7a8d917fd7b8e2f48ba265
Aleksandr Juncevich <juncevich@tochka.com>
2017-08-23 23:43:35 +0500

    Initial commit

---
+++ src/main/java/com/relay/RelaySystemApplication.java
@@ -0,0 +1,12 @@
+package com.relay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelaySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelaySystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/relay/RelaySystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.relay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RelaySystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
