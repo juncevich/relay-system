001887baebf4e07dd68ff322523538625685ed97
juncevich <a.juncevich@gmail.com>
2017-08-25 00:31:27 +0500

    Add simple rest controller

--- src/main/java/com/relay/RelaySystemApplication.java
+++ src/main/java/com/relay/RelaySystemApplication.java
@@ -2,11 +2,19 @@ package com.relay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class RelaySystemApplication {
+@SpringBootApplication @RestController public class RelaySystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RelaySystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(RelaySystemApplication.class, args);
+    }
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET) public String getHello() {
+
+        return "Hello!";
+    }
 }
