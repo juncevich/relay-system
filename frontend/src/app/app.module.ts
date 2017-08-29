18c902b05360ebc9670ed441a8c317c0f750b967
juncevich <a.juncevich@gmail.com>
2017-08-29 20:08:10 +0500

    Add bootstrap. Add header menu.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RelayItemComponent } from './relay-item/relay-item.component';
 import { RelaysComponent } from './relays/relays.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RelayItemComponent,
-    RelaysComponent
+    RelaysComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
