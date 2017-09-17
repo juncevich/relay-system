4fee3a5a38a2cf769f89f6defb81232c17172eb4
juncevich <a.juncevich@gmail.com>
2017-09-17 17:05:23 +0500

    Rename relays

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RelayItemComponent } from './relay-list/relay-item/relay-item.component';
-import { RelaysComponent } from './relay-list/relays/relays.component';
+import { RelayItemComponent } from './relays/relay-item/relay-item.component';
+import { RelaysComponent } from './relays/relay-detail/relays.component';
 import { HeaderComponent } from './header/header.component';
 import { StativComponent } from './stativ/stativ.component';
 import { LeftMenuComponent } from './left-menu/left-menu.component';
 import { BreadcumbComponent } from './breadcumb/breadcumb.component';
-import { RelayListComponent } from './relay-list/relay-list.component';
+import { RelayListComponent } from './relays/relay-list.component';
 
 @NgModule({
   declarations: [
