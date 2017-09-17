1adf0e5cf5ab07e39ea5cc4a9221d0b4dec15ed0
juncevich <a.juncevich@gmail.com>
2017-09-17 18:32:14 +0500

    Move header and left menu to core

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,9 +2,9 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {HeaderComponent} from './header/header.component';
+import {HeaderComponent} from './core/header/header.component';
 import {StativComponent} from './stativ/stativ.component';
-import {LeftMenuComponent} from './left-menu/left-menu.component';
+import {LeftMenuComponent} from './core/left-menu/left-menu.component';
 import {BreadcumbComponent} from './breadcumb/breadcumb.component';
 import {RelaysModule} from './relays/relays.module';
 
