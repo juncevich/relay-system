effbb704457da88d1cc69fa0bb4766db813338a8
juncevich <a.juncevich@gmail.com>
2017-10-10 01:14:21 +0500

    Add retrieveRelay method

--- backend/src/main/java/com/relay/controllers/RelayController.java
+++ backend/src/main/java/com/relay/controllers/RelayController.java
@@ -4,9 +4,11 @@ import com.relay.model.Relay;
 import com.relay.service.RelayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class RelayController {
@@ -21,5 +23,10 @@ public class RelayController {
     @GetMapping("/relays")
     public List<Relay> retrieveAllRelays() {
         return relayService.findAll();
+    }
+
+    @GetMapping("/relays/{id}")
+    public Optional<Relay> retrieveRelay(@PathVariable int id) {
+        return relayService.findOne(id);
     }
 }
