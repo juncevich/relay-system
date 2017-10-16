510fcfca2434514f5a44fde4396f839510e7875f
juncevich <a.juncevich@gmail.com>
2017-10-17 00:47:50 +0500

    Add user repository + user controller

---
+++ backend/src/main/java/com/relay/controllers/UserController.java
@@ -0,0 +1,65 @@
+package com.relay.controllers;
+
+import com.relay.exeptions.RelayNotFoundException;
+import com.relay.model.Relay;
+import com.relay.model.User;
+import com.relay.repository.UserRepository;
+import com.relay.service.RelayService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+
+    private final RelayService relayService;
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(RelayService relayService, UserRepository userRepository) {
+        this.relayService = relayService;
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllRelays() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public Resource<User> retrieveRelay(@PathVariable int id) {
+        Optional<User> user = userRepository.findById(id);
+        if (!user.isPresent())
+            throw new RelayNotFoundException("id - " + id);
+
+        Resource<User> resource = new Resource<>(user.get());
+
+        ControllerLinkBuilder linkTo = ControllerLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(this.getClass()).retrieveAllRelays());
+        resource.add(linkTo.withRel("all-users"));
+        return resource;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<Object> createRelay(@Valid @RequestBody Relay relay) {
+        Relay savedRelay = relayService.save(relay);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedRelay.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public Relay deleteRelay(@PathVariable int id) {
+        Relay relay = relayService.deleteById(id);
+        if (relay == null)
+            throw new RelayNotFoundException("id - " + id);
+        return relay;
+    }
+}
---
+++ backend/src/main/java/com/relay/service/UserService.java
@@ -0,0 +1,53 @@
+package com.relay.service;
+
+import com.relay.model.Relay;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Component
+public class UserService {
+
+    private static final List<Relay> relays = new ArrayList<>();
+
+    private static long relayCount = 4;
+
+    static {
+        relays.add(new Relay(1L, "first"));
+        relays.add(new Relay(2L, "second"));
+        relays.add(new Relay(3L, "third"));
+        relays.add(new Relay(4L, "fourth"));
+    }
+
+    public List<Relay> findAll() {
+        return relays;
+    }
+
+    public Relay save(Relay relay) {
+        if (relay.getId() == null)
+            relay.setId(++relayCount);
+        relays.add(relay);
+        return relay;
+    }
+
+    public Relay findOne(long id) {
+        return relays.stream().filter(relay -> relay.getId() == id).findFirst().orElse(null);
+    }
+
+    public Relay deleteById(long id) {
+        Iterator<Relay> it = relays.iterator();
+        while (it.hasNext()) {
+            Relay relay = it.next();
+            if (relay.getId() == id) {
+                it.remove();
+                return relay;
+            }
+
+        }
+        return null;
+    }
+
+
+}
