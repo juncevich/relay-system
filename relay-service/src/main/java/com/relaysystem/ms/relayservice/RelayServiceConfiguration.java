0dc7e979d98a5d7ba0a4b9cb9466120dc079f056
juncevich <a.juncevich@gmail.com>
2017-10-18 00:33:34 +0500

    Fix misstypes

--- relay-service/src/main/java/com/relaysystem/ms/relayservice/RelayServiceConfiguration.java
+++ relay-service/src/main/java/com/relaysystem/ms/relayservice/RelayServiceConfiguration.java
@@ -1,10 +1,13 @@
 package com.relaysystem.ms.relayservice;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
 
 @Data
+@AllArgsConstructor
 public class RelayServiceConfiguration {
+
     @NonNull
     private String region;
 }
