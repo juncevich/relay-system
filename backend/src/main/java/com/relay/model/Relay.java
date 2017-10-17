4f95b1d74100fb5b868062d21f58bbc2347de646
juncevich <a.juncevich@gmail.com>
2017-10-17 19:01:09 +0500

    Add station entity and relation between sation and relay

--- backend/src/main/java/com/relay/model/Relay.java
+++ backend/src/main/java/com/relay/model/Relay.java
@@ -7,16 +7,24 @@ import lombok.Data;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
+import javax.persistence.*;
 import javax.validation.constraints.Size;
 
+@Entity
 @Data
 @AllArgsConstructor
 @RequiredArgsConstructor
 @ApiModel(description = "Relay details.")
 public class Relay {
+
+    @Id
+    @GeneratedValue
     private Long id;
     @NonNull
     @Size(min = 2, message = "Text should contain at least to characters")
     @ApiModelProperty(notes = "Text should contain at least to characters")
     private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Station station;
 }
--- backend/src/main/java/com/relay/model/Station.java
+++ backend/src/main/java/com/relay/model/Station.java
@@ -1,4 +1,24 @@
 package com.relay.model;
 
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+@Data
 public class Station {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "station")
+    private List<Relay> relay;
 }
