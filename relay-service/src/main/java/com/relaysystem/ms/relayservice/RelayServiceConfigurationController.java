d4e9446ba998a652a84a62d2c70cea1cb913a346
juncevich <a.juncevich@gmail.com>
2017-10-17 23:09:44 +0500

    Add config via properties

--- relay-service/src/main/java/com/relaysystem/ms/relayservice/RelayServiceConfigurationController.java
+++ relay-service/src/main/java/com/relaysystem/ms/relayservice/RelayServiceConfigurationController.java
@@ -1,12 +1,20 @@
 package com.relaysystem.ms.relayservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RelayServiceConfigurationController {
+    private final Configuration configuration;
+
+    @Autowired
+    public RelayServiceConfigurationController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
     @GetMapping("/region")
     public RelayServiceConfiguration retrieveInfoFromConfiguration() {
-        return new RelayServiceConfiguration("svrd");
+        return new RelayServiceConfiguration(configuration.getRegion());
     }
 }
