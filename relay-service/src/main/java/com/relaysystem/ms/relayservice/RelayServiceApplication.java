e7d29d9685ce24489849a11bf53f378107e3268d
juncevich <a.juncevich@gmail.com>
2017-10-17 22:12:02 +0500

    Add relay service

---
+++ relay-service/src/main/java/com/relaysystem/ms/relayservice/RelayServiceApplication.java
@@ -0,0 +1,12 @@
+package com.relaysystem.ms.relayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelayServiceApplication.class, args);
+	}
+}
