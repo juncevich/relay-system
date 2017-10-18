6570cd6ee8d114f1a46433ce5c7a8940b677096d
juncevich <a.juncevich@gmail.com>
2017-10-18 22:17:06 +0500

    add relay-change service

---
+++ relay-change-service/src/main/java/com/relaysystem/ms/relaychangeservice/RelayChangeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.relaysystem.ms.relaychangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelayChangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelayChangeServiceApplication.class, args);
+	}
+}
