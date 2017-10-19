35a0678db3f5d7c7d3ba498f98cbd14b8a128abb
juncevich <a.juncevich@gmail.com>
2017-10-19 23:33:57 +0500

    add relay-convert-service

---
+++ relay-convert-service/src/main/java/com/relaysystem/ms/relayconvertservice/RelayConvertServiceApplication.java
@@ -0,0 +1,12 @@
+package com.relaysystem.ms.relayconvertservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelayConvertServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelayConvertServiceApplication.class, args);
+	}
+}
