8743a0e108094d58ac79baf271971263fc660367
juncevich <a.juncevich@gmail.com>
2017-10-20 23:36:06 +0500

    Add feign + start tune it

--- relay-convert-service/src/main/java/com/relaysystem/ms/relayconvertservice/RelayConvertServiceApplication.java
+++ relay-convert-service/src/main/java/com/relaysystem/ms/relayconvertservice/RelayConvertServiceApplication.java
@@ -2,8 +2,10 @@ package com.relaysystem.ms.relayconvertservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.relaysystem.ms.relayconvertservice")
 public class RelayConvertServiceApplication {
 
 	public static void main(String[] args) {
