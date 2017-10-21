0233f5f39bbbe17e4567ee2d363a154fae395d5f
juncevich <a.juncevich@gmail.com>
2017-10-21 09:31:17 +0500

    Add ribbon

--- relay-convert-service/src/main/java/com/relaysystem/ms/relayconvertservice/RelayExchangeServiceProxy.java
+++ relay-convert-service/src/main/java/com/relaysystem/ms/relayconvertservice/RelayExchangeServiceProxy.java
@@ -1,12 +1,15 @@
 package com.relaysystem.ms.relayconvertservice;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "relay-exchange-service", url = "localhost:8000")
+//@FeignClient(name = "relay-exchange-service", url = "localhost:8000")
+@FeignClient(name = "relay-exchange-service")
+@RibbonClient(name = "relay-exchange-service")
 public interface RelayExchangeServiceProxy {
 
     @GetMapping("/relay-exchange/from/{from}/to/{to}")
-    public RelayConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
+    RelayConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 }
