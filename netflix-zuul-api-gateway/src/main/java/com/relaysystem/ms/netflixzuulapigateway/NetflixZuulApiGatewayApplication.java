600f51c26e345675bfa5952f07ad00c4be5b248f
juncevich <a.juncevich@gmail.com>
2017-10-22 20:17:08 +0500

    Add sleuth

--- netflix-zuul-api-gateway/src/main/java/com/relaysystem/ms/netflixzuulapigateway/NetflixZuulApiGatewayApplication.java
+++ netflix-zuul-api-gateway/src/main/java/com/relaysystem/ms/netflixzuulapigateway/NetflixZuulApiGatewayApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulServer
@@ -12,5 +14,10 @@ public class NetflixZuulApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NetflixZuulApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public AlwaysSampler defaultSampler() {
+        return new AlwaysSampler();
     }
 }
