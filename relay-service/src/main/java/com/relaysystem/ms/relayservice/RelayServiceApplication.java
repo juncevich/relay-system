e0cfa8918d80b5ecd0081ddcdf269a257a0ad45d
juncevich <a.juncevich@gmail.com>
2017-10-22 22:48:46 +0500

    Add hystrix

--- relay-service/src/main/java/com/relaysystem/ms/relayservice/RelayServiceApplication.java
+++ relay-service/src/main/java/com/relaysystem/ms/relayservice/RelayServiceApplication.java
@@ -2,11 +2,13 @@ package com.relaysystem.ms.relayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+@EnableHystrix
 @SpringBootApplication
 public class RelayServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RelayServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RelayServiceApplication.class, args);
+    }
 }
