54844ed2560dd5f935f435c12110f8d68fd3f0e0
juncevich <a.juncevich@gmail.com>
2017-10-23 23:48:57 +0500

    Remove unnescessary annotation

--- relay-convert-service/src/main/java/com/relaysystem/ms/relayconvertservice/RelayExchangeServiceProxy.java
+++ relay-convert-service/src/main/java/com/relaysystem/ms/relayconvertservice/RelayExchangeServiceProxy.java
@@ -5,7 +5,6 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-//@FeignClient(name = "relay-exchange-service", url = "localhost:8000")
 @FeignClient(name = "netflix-zuul-api-gateway")
 @RibbonClient(name = "relay-exchange-service")
 public interface RelayExchangeServiceProxy {
