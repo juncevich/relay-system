c15826986234f2d2bb5e540b5f7e685b004aafd4
juncevich <a.juncevich@gmail.com>
2017-10-25 23:27:52 +0500

    fir mistake

--- backend/src/main/java/com/relay/RelaySystemApplication.java
+++ backend/src/main/java/com/relay/RelaySystemApplication.java
@@ -2,10 +2,13 @@ package com.relay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.convert.Jsr310Converters;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EntityScan(basePackageClasses = { RelaySystemApplication.class, Jsr310Converters.class })
 public class RelaySystemApplication {
 
     public static void main(String[] args) {
