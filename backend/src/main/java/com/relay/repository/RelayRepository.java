c501d5ca3fe56305f056b75c81d912acceab2f94
juncevich <a.juncevich@gmail.com>
2017-10-27 23:34:49 +0500

    Add findByText method

--- backend/src/main/java/com/relay/repository/RelayRepository.java
+++ backend/src/main/java/com/relay/repository/RelayRepository.java
@@ -1,10 +1,15 @@
 package com.relay.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.relay.model.Relay;
 
 @Repository
 public interface RelayRepository extends JpaRepository<Relay, Long> {
+
+    List<Relay> findByText(@Param("text") String text);
 }
