8b8997ff834854c02c57d683fd0fb581d4af8d4d
juncevich <a.juncevich@gmail.com>
2017-11-07 23:51:23 +0500

    Test commit after reinstall system

--- backend/src/main/java/com/relay/RelaySystemApplication.java
+++ backend/src/main/java/com/relay/RelaySystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-// @EntityScan(basePackageClasses = { RelaySystemApplication.class, Jsr310Converters.class })
 //@EnableDiscoveryClient
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {RelaySystemApplication.class, Jsr310Converters.class})
 public class RelaySystemApplication {
 
     public static void main(String[] args) {
