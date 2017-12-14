ec9796a1d4929e8266713b7a52bb940df62e0628
juncevich <a.juncevich@gmail.com>
2017-12-15 00:28:50 +0500

    Add fish method to test.

--- backend/src/test/java/com/relay/repository/StationRepositoryTest.java
+++ backend/src/test/java/com/relay/repository/StationRepositoryTest.java
@@ -1,9 +1,15 @@
 package com.relay.repository;
 
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class StationRepositoryTest extends AbsrtactRepositoryTest {
 
     @Autowired
     StationRepository stationRepository;
+
+    @Test
+    public void test() {
+
+    }
 }
