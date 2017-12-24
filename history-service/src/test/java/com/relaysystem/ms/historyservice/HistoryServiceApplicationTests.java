f30e994d024bdb9d7447c072deda557f1351c459
juncevich <a.juncevich@gmail.com>
2017-12-24 18:09:15 +0500

    Add relay history service.

---
+++ history-service/src/test/java/com/relaysystem/ms/historyservice/HistoryServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.relaysystem.ms.historyservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HistoryServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
