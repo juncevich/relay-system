1c7a7afccd71b4819c5d80ed62a2c72ef6100c08
juncevich <a.juncevich@gmail.com>
2018-03-01 23:46:59 +0500

    Remove unnescessary mongo config

--- backend/src/main/java/com/relay/config/ReactiveMongoConfig.java
+++ backend/src/main/java/com/relay/config/ReactiveMongoConfig.java
@@ -6,19 +6,6 @@ import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRep
 @EnableReactiveMongoRepositories("com.relay.repository")
 @Configuration
 public class ReactiveMongoConfig
-// extends AbstractReactiveMongoConfiguration
 {
 
-    // @Value("${spring.data.mongodb.database}")
-    // String mongoUri;
-    //
-    // @Override
-    // public MongoClient reactiveMongoClient() {
-    // return MongoClients.create(mongoUri);
-    // }
-    //
-    // @Override
-    // protected String getDatabaseName() {
-    // return "jsa_mongodb";
-    // }
 }
