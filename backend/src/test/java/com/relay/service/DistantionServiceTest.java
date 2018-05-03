cf90590880a5d74c94c9a04e9e7c0d2804a17c3e
juncevich <a.juncevich@gmail.com>
2018-05-04 02:03:34 +0500

    Some dirty code

--- backend/src/test/java/com/relay/service/DistantionServiceTest.java
+++ backend/src/test/java/com/relay/service/DistantionServiceTest.java
@@ -1,27 +1,39 @@
 package com.relay.service;
 
-import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.assertEquals;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.relay.model.Distantion;
 import com.relay.repository.AbstractRepositoryTest;
+import com.relay.repository.DistantionRepository;
 
 public class DistantionServiceTest extends AbstractRepositoryTest {
 
     @Autowired
     private DistantionService distantionService;
 
+    @Autowired
+    private DistantionRepository distantionRepository;
+
+    @Override
+    @Before
+    public void setUp() {
+
+        distantionRepository.deleteAll().subscribe();
+    }
+
     @Test
     public void findAll() {
 
         List<Distantion> emptyDistantionList =
                 distantionService.findAll().collect(Collectors.toList()).block();
-        assertTrue(emptyDistantionList.isEmpty());
+        assertEquals(0, emptyDistantionList.size());
     }
 
     @Test
