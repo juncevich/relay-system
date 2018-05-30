63773cdf379939d10f6a65841c546e9d830f225b
juncevich <a.juncevich@gmail.com>
2018-05-30 22:50:35 +0500

    Fix Security.

--- backend/src/main/java/com/relay/config/WebSecurityConfig.java
+++ backend/src/main/java/com/relay/config/WebSecurityConfig.java
@@ -4,36 +4,41 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
+// @EnableWebSecurity
 public class WebSecurityConfig {
 
-    // @Bean
-    // public UserDetailsService userDetailsService() {
-    //
-    // InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-    // manager.createUser(User.withUsername("user").password("password").roles("USER").build());
-    // manager.createUser(
-    // User.withUsername("admin").password("password").roles("USER", "ADMIN").build());
-    // return manager;
-    // }
+    @Bean
+    public UserDetailsService userDetailsService() {
 
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(
+                User.withUsername("user").password("{noop}password").roles("USER").build());
+        manager.createUser(User.withUsername("admin").password("{noop}password")
+                .roles("USER", "ADMIN").build());
+        return manager;
+    }
+    //
     // @Autowired
     // public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
     //
     // auth.inMemoryAuthentication().withUser("user") // #1
-    // .password("password").roles("USER").and().withUser("admin") // #2
-    // .password("password").roles("ADMIN", "USER");
+    // .password("{noop}password").roles("USER").and().withUser("admin") // #2
+    // .password("{noop}password").roles("ADMIN", "USER");
     // }
 
     @Bean
-    public MapReactiveUserDetailsService userDetailsRepository() {
+    public ReactiveUserDetailsService userDetailsRepository() {
 
-        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password")
-                .roles("USER").build();
+        UserDetails user =
+                User.withUsername("user").password("{noop}password").roles("USER").build();
         return new MapReactiveUserDetailsService(user);
     }
 
@@ -50,5 +55,6 @@ public class WebSecurityConfig {
         // return http.build();
         return http.authorizeExchange().anyExchange().permitAll().and().build();
     }
+
 
 }
