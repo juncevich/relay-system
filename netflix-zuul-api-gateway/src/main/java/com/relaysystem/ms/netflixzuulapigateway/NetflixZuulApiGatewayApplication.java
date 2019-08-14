b864d415b39bfb13fc65cf51afe9bb1d2e1137f2
Alexandr Juncevich <a.juncevich@gmail.com>
2019-08-14 21:25:49 +0300

    Add zuul proxy

--- netflix-zuul-api-gateway/src/main/java/com/relaysystem/ms/netflixzuulapigateway/NetflixZuulApiGatewayApplication.java
+++ netflix-zuul-api-gateway/src/main/java/com/relaysystem/ms/netflixzuulapigateway/NetflixZuulApiGatewayApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-
-import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -18,9 +15,4 @@ public class NetflixZuulApiGatewayApplication {
         SpringApplication.run(NetflixZuulApiGatewayApplication.class, args);
     }
 
-    @Bean
-    public Sampler defaultSampler() {
-
-        return Sampler.ALWAYS_SAMPLE;
-    }
 }
