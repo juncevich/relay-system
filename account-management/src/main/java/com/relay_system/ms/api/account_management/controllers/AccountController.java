ae5da2345e62835cf0df19c1c237b9b2b01b79fe
Alexandr Juncevich <a.juncevich@gmail.com>
2019-08-14 22:00:46 +0300

    Add load balance check

--- account-management/src/main/java/com/relay_system/ms/api/account_management/controllers/AccountController.java
+++ account-management/src/main/java/com/relay_system/ms/api/account_management/controllers/AccountController.java
@@ -1,13 +1,18 @@
 package com.relay_system.ms.api.account_management.controllers;
 
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.core.env.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/account")
 public class AccountController {
 
+    @Autowired
+    Environment env;
+
     @GetMapping("/status/check")
     public String status() {
-        return "Working ...";
+        return "Working on port " + env.getProperty("local.server.port");
     }
 }
