a6056dbcecdb457c4752ab8d795acedd0926464a
Alexandr Juncevich <a.juncevich@gmail.com>
2019-08-18 19:04:01 +0300

    Add gateway allowed ip

--- user-service/src/main/java/com/relaysystem/ms/users/security/WebSecurity.java
+++ user-service/src/main/java/com/relaysystem/ms/users/security/WebSecurity.java
@@ -1,6 +1,7 @@
 package com.relaysystem.ms.users.security;
 
 import org.springframework.context.annotation.*;
+import org.springframework.core.env.*;
 import org.springframework.security.config.annotation.web.builders.*;
 import org.springframework.security.config.annotation.web.configuration.*;
 
@@ -8,10 +9,17 @@ import org.springframework.security.config.annotation.web.configuration.*;
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
+    Environment environment;
+
+    public WebSecurity(Environment environment) {
+        this.environment = environment;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.headers().frameOptions().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"));
+
     }
 }
