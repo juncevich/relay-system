3982e7c5e8e709a54702706b9b8a3f667ce4c5d5
Alex Juncevich <a.juncevich@gmail.com>
2019-08-28 21:59:51 +0300

    Enable web security in Zuul

---
+++ netflix-zuul-api-gateway/src/main/java/com/relaysystem/ms/netflixzuulapigateway/security/WebSecurity.java
@@ -0,0 +1,25 @@
+package com.relaysystem.ms.netflixzuulapigateway.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+    private final Environment environment;
+
+    public WebSecurity(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}
