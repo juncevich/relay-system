6f426486c7861eba3932769894c5a1b7f53b8258
Alexandr Juncevich <a.juncevich@gmail.com>
2019-09-09 23:02:47 +0300

    Add actuator routes to gateway

--- netflix-zuul-api-gateway/src/main/java/com/relaysystem/ms/netflixzuulapigateway/security/WebSecurity.java
+++ netflix-zuul-api-gateway/src/main/java/com/relaysystem/ms/netflixzuulapigateway/security/WebSecurity.java
@@ -21,8 +21,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         http.headers().frameOptions().disable();
         http.authorizeRequests()
-                .antMatchers(HttpMethod.POST, environment.getProperty("api.zuul.actuator.url.path")).permitAll()
-                .antMatchers(HttpMethod.POST, environment.getProperty("api.h2console.url.path")).permitAll()
+                .antMatchers(environment.getProperty("api.zuul.actuator.url.path")).permitAll()
+                .antMatchers(environment.getProperty("api.h2console.url.path")).permitAll()
                 .antMatchers(HttpMethod.POST, environment.getProperty("api.registration.url.path")).permitAll()
                 .antMatchers(HttpMethod.POST, environment.getProperty("api.login.url.path")).permitAll()
                 .anyRequest().authenticated()
