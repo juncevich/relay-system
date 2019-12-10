ecc27247f9f99c758a91712b4ff9eacc6fda036f
Alexandr Juncevich <a.juncevich@gmail.com>
2019-12-10 23:28:22 +0300

    Try to fix test

--- rs-discovery/src/test/java/com/relaysystem/ms/discovery/NetflixEurekaNamingServerApplicationTests.java
+++ rs-discovery/src/test/java/com/relaysystem/ms/discovery/NetflixEurekaNamingServerApplicationTests.java
@@ -1,10 +1,11 @@
-package com.relaysystem.ms.netflixeurekanamingserver;
+package com.relaysystem.ms.discovery;
 
 import org.junit.*;
 import org.junit.runner.*;
 import org.springframework.boot.test.context.*;
 import org.springframework.test.context.junit4.*;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class NetflixEurekaNamingServerApplicationTests {
 
