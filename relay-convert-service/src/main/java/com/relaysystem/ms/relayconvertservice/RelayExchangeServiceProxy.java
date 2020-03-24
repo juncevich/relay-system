5c0f870a34fad92a1c1a086cc89dcdccfe29e0d9
Alexandr Juncevich <a.juncevich@gmail.com>
2020-03-24 23:08:26 +0300

    Rename project to cloud-api-gateway

--- relay-convert-service/src/main/java/com/relaysystem/ms/relayconvertservice/RelayExchangeServiceProxy.java
+++ relay-convert-service/src/main/java/com/relaysystem/ms/relayconvertservice/RelayExchangeServiceProxy.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "netflix-zuul-api-gateway")
+@FeignClient(name = "cloud-api-gateway")
 @RibbonClient(name = "relay-exchange-service")
 public interface RelayExchangeServiceProxy {
 
