efb0dafd1c3a556b5865aa36c9fc72cb47f94e07
Alexandr Juncevich <a.juncevich@gmail.com>
2020-07-22 10:33:02 +0300

    Add sidecar for postgres

---
+++ sidecar-postgres/src/main/java/com/relaysystem/ms/sidecar/postgres/PostgresSidecarApplication.java
@@ -0,0 +1,13 @@
+package com.relaysystem.ms.sidecar.postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresSidecarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostgresSidecarApplication.class, args);
+    }
+
+}
---
+++ sidecar-postgres/src/test/java/com/relaysystem/ms/sidecar/postgres/PostgresSidecarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relaysystem.ms.sidecar.postgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresSidecarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
