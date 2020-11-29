b00c57f01d8bdcfbff0c6ef7a51a92dd06b4da27
Alexandr Juncevich <a.juncevich@gmail.com>
2020-11-29 18:47:11 +0300

    Add dekorate to data-service

--- data-service/src/main/java/com/relay/RelaySystemApplication.java
+++ data-service/src/main/java/com/relay/RelaySystemApplication.java
@@ -3,7 +3,10 @@ package com.relay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.dekorate.kubernetes.annotation.KubernetesApplication;
+
 // @EnableDiscoveryClient
+@KubernetesApplication
 @SpringBootApplication
 public class RelaySystemApplication {
 
