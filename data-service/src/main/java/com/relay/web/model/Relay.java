0754856b21801a0898905a8f641b75d938401a40
Alexandr Juncevich <a.juncevich@gmail.com>
2021-01-24 22:36:15 +0300

    Add records

--- data-service/src/main/java/com/relay/web/model/Relay.java
+++ data-service/src/main/java/com/relay/web/model/Relay.java
@@ -2,37 +2,19 @@ package com.relay.web.model;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.*;
+import lombok.NonNull;
 
 import javax.validation.constraints.Size;
 import java.time.OffsetDateTime;
 
 @ApiModel(description = "Relay details.")
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class Relay {
-
-    /**
-     * Date of manufacture relay
-     */
-    @ApiModelProperty(notes = "Date of manufacture relay")
-    private OffsetDateTime dateOfManufacture;
-
-    /**
-     * Relay verification date
-     */
-    @ApiModelProperty(notes = "Verification date relay")
-    private OffsetDateTime verificationDate;
-
-    /**
-     * Relay serial number
-     */
-    @ApiModelProperty(notes = "Serial number relay")
-    @NonNull
-    @Size(min = 5,
-            message = "Serial number should contain at least five characters")
-    private String serialNumber;
-
+public record Relay(@ApiModelProperty(notes = "Date of manufacture relay")
+                    OffsetDateTime dateOfManufacture,
+                    @ApiModelProperty(notes = "Verification date relay")
+                    OffsetDateTime verificationDate,
+                    @ApiModelProperty(notes = "Serial number relay")
+                    @NonNull
+                    @Size(min = 5, message = "Serial number should contain at least five characters")
+                    String serialNumber
+) {
 }
