29af42ab3f0fbb47dd7af993b46d7e9756732262
Alexandr Juncevich <a.juncevich@gmail.com>
2021-04-22 22:33:38 +0300

    Add comment service

---
+++ comments-service/src/test/kotlin/com/relay/commentsservice/CommentsServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.relay.commentsservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommentsServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
