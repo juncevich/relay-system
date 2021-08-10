3637475a93fe20af9e963250b24da796936acd03
Alexandr Juncevich <a.juncevich@gmail.com>
2021-08-10 23:50:42 +0300

    Remove unnecessay projects

--- sidecar-postgres/src/main/java/com/relaysystem/ms/sidecar/postgres/healthcheck/SidecarHealthIndicator.java
+++
@@ -1,6 +0,0 @@
-package com.relaysystem.ms.sidecar.postgres.healthcheck;
-
-import org.springframework.boot.actuate.health.HealthIndicator;
-
-public interface SidecarHealthIndicator extends HealthIndicator {
-}
