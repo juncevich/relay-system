6d1f27eb590bfec2edaade6ec981066f70fac6b0
Alexandr Juncevich <a.juncevich@gmail.com>
2022-06-14 23:02:40 +0300

    Minor changes

--- account-management/src/main/java/com/relay_system/ms/api/account_management/controllers/AccountController.java
+++ account-management/src/main/java/com/relay_system/ms/api/account_management/controllers/AccountController.java
@@ -1,15 +1,19 @@
 package com.relay_system.ms.api.account_management.controllers;
 
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.core.env.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/account")
 public class AccountController {
 
-    @Autowired
-    Environment env;
+    final Environment env;
+
+    public AccountController(Environment env) {
+        this.env = env;
+    }
 
     @GetMapping("/status/check")
     public String status() {
