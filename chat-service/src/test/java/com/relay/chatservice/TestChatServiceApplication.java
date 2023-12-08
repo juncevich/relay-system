fc7f5a7dc4e2629cf465230c78d291da4cd35561
Alexandr Juncevich <a.juncevich@gmail.com>
2023-12-09 00:43:35 +0300

    Add mongo container + up gradle version

--- chat-service/src/test/java/com/relay/chatservice/TestChatServiceApplication.java
+++ chat-service/src/test/java/com/relay/chatservice/TestChatServiceApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestChatServiceApplication {
@@ -26,6 +28,12 @@ public class TestChatServiceApplication {
     @ServiceConnection(name = "openzipkin/zipkin")
     GenericContainer<?> zipkinContainer() {
         return new GenericContainer<>("openzipkin/zipkin:latest").withExposedPorts(9411);
+    }
+
+    @Bean
+    @ServiceConnection
+    public MongoDBContainer mongoDBContainer() {
+        return new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
     }
 
     public static void main(String[] args) {
