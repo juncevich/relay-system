587c999fd1033f689ab5d4996f48dc30e2577a6e
Alexandr Juncevich <a.juncevich@gmail.com>
2023-12-10 16:25:33 +0300

    Disable dependencies

--- chat-service/src/test/java/com/relay/chatservice/TestChatServiceApplication.java
+++ chat-service/src/test/java/com/relay/chatservice/TestChatServiceApplication.java
@@ -6,34 +6,34 @@ import org.springframework.boot.testcontainers.service.connection.ServiceConnect
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
+//import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestChatServiceApplication {
 
-    @Bean
-    @ServiceConnection
-    PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>("postgres:latest");
-    }
+//    @Bean
+//    @ServiceConnection
+//    PostgreSQLContainer<?> postgresContainer() {
+//        return new PostgreSQLContainer<>("postgres:latest");
+//    }
 
-    @Bean
-    @ServiceConnection(name = "redis")
-    GenericContainer<?> redisContainer() {
-        return new GenericContainer<>("redis:latest").withExposedPorts(6379);
-    }
-
-    @Bean
-    @ServiceConnection(name = "openzipkin/zipkin")
-    GenericContainer<?> zipkinContainer() {
-        return new GenericContainer<>("openzipkin/zipkin:latest").withExposedPorts(9411);
-    }
+//    @Bean
+//    @ServiceConnection(name = "redis")
+//    GenericContainer<?> redisContainer() {
+//        return new GenericContainer<>("redis:latest").withExposedPorts(6379);
+//    }
+//
+//    @Bean
+//    @ServiceConnection(name = "openzipkin/zipkin")
+//    GenericContainer<?> zipkinContainer() {
+//        return new GenericContainer<>("openzipkin/zipkin:latest").withExposedPorts(9411);
+//    }
 
     @Bean
     @ServiceConnection
     public MongoDBContainer mongoDBContainer() {
-        return new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
+        return new MongoDBContainer(DockerImageName.parse("mongo:latest"));
     }
 
     public static void main(String[] args) {
