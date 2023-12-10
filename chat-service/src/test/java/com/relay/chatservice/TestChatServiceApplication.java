b883f38e4de538e03aa2c017b66fe42dff351018
Alexandr Juncevich <a.juncevich@gmail.com>
2023-12-10 17:12:43 +0300

    Fix container start

--- chat-service/src/test/java/com/relay/chatservice/TestChatServiceApplication.java
+++ chat-service/src/test/java/com/relay/chatservice/TestChatServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MongoDBContainer;
-//import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
+
+import java.util.HashMap;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestChatServiceApplication {
@@ -33,7 +33,12 @@ public class TestChatServiceApplication {
     @Bean
     @ServiceConnection
     public MongoDBContainer mongoDBContainer() {
-        return new MongoDBContainer(DockerImageName.parse("mongo:latest"));
+        var env = new HashMap<String, String>();
+//        env.put("MONGO_INITDB_ROOT_USERNAME", "alex");
+//        env.put("MONGO_INITDB_ROOT_PASSWORD", "Password12345");
+        env.put("MONGO_DB", "test");
+        env.put("MONGO_INITDB_DATABASE", "test");
+        return new MongoDBContainer(DockerImageName.parse("mongo:latest")).withEnv(env).withExposedPorts(27017);
     }
 
     public static void main(String[] args) {
