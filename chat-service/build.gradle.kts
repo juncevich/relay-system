538efc2e5c8adc34eb9445cc68830457c457715b
Alexandr Juncevich <a.juncevich@gmail.com>
2025-01-19 00:40:32 +0300

    Update spring boot version

--- chat-service/build.gradle.kts
+++ chat-service/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.4.0"
-    id("io.spring.dependency-management") version "1.1.6"
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
 //    id("org.graalvm.buildtools.native") version "0.9.20"
 //    id("org.springframework.cloud.contract") version "4.0.3"
 
